92fcbf18a2aeb0d3dbfca3fe99ee8e9adb4ab293
Brandon-Magofna <magofna68@gmail.com>
2021-12-16 14:47:57 -0800

    :hammer: out data sets and staging to be exported: poem 1

---
+++ src/data.js
@@ -0,0 +1,89 @@
+
+
+export const data = [
+ {
+     id: 1,
+     title: "The Oath of Promise",
+     alt: "",
+     img: "https://d2r55xnwy6nx47.cloudfront.net/uploads/2018/01/BigBounce_2880x1220.gif",
+     text: `"The silence, it envelopes me.
+        her joyous smile and loving embrace,
+        A forsaken pleasure lost in the sands of illusion.
+        At the flick of an eye, we're back at Resolution.
+        I realized As you got lost in the music, lost in the night,
+        I want to walk by your side, hand in hand through my life
+        You brighten my day, you're my morning sun,
+        Moon of my life, what have I done?
+        The ball drops, as I drop the ball,
+        I failed to appreciate you all in all.
+        Like a precious gem, you stole my heart,
+        In loving embrace, You deserve a new start,
+        Your my candle in darkness, my guiding light,
+        so be free my love, I'll let go without fight,
+        I pray you bless the world with your smile,
+        Live, laugh, & love; meanwhile,
+        I make you this promise-as of tonight,
+        To love & respect you, to do what is right,
+        once you've recovered and regained your sight,
+        Should you choose to return, I'll wait day and night.
+        I recognize now, that night at Resolution,
+        A perfect depiction; I chose you as my solution
+        A forsaken pleasure lost in the sands of illusion.
+        her joyous smile and loving embrace,
+        The silence; it envelop's me."`
+ },
+ {
+    id: 2,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 3,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 4,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 5,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 6,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 7,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 8,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 9,
+    title: "",
+    alt: "",
+    img: "",
+},
+{
+    id: 10,
+    title: "",
+    alt: "",
+    img: "",
+},
+]
