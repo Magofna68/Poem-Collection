a4a647e30710eeda6242c03238c7fe518dd30c35
Brandon-Magofna <magofna68@gmail.com>
2021-12-16 15:00:23 -0800

    :sparkles: poem added: 2 & 3

--- src/data.js
+++ src/data.js
@@ -34,15 +34,56 @@ export const data = [
  },
  {
     id: 2,
-    title: "",
-    alt: "",
+    title: "Mistakes",
+    date: "",
     img: "",
+    text: `"Waves of sorrow wash over my soul
+    As my heart plunges into a steep abyss.
+    Mankind’s strength of memory falls short of grandeur as sickening misgiving acts repeat as the tormenting slaughter eats at my being.
+    It feeds through my body, encompassing all space, and slowly depleting my soul of its inner nature. The havoc lays siege to my chest, numbing all prior signs of emergency.
+    I clutch in despair to relished memories, only to have them further extinguished by the blazing fires of mistakes.
+    My selfishness and self pity quickly overtook respect and loving embrace. My greatest strength goes without optimal utilization; my sheet pure as light.
+    Living an Unscrutinized life, my failings spiral, losing the thing most precious to me. In this slump I present myself, I need elevation. Something to prop me up from my station.
+    I am enlightened by every spot that went wrong,
+    but the ending to our story has already begun,
+    I am undone by my actions, undone by my thoughts,
+    I am a lone astronaut left to suffer my fate in the face of the the immeasurable vastness of space.
+    I slowly slip into darkness; no salvation, canvas of chaos seizes life through creation,
+    I am the master of puppets pulling strings behind scene, I am the artist behind brush who’s scheme turned routine.
+    I burned my bridge, lost my sight,
+    through the clouds of smoke I’ll continue the fight,
+    To right my wrongs, a story unfinished,
+    But perhaps my time is already finished."`
 },
 {
     id: 3,
-    title: "",
-    alt: "",
+    title: "Unbeatable",
+    date: "8th Grade, 2008",
     img: "",
+    text: `"15 years I fought to win,
+    15 years I thought i’d sinned,
+    a count of 8 till this very day,
+    is the amount of times, i’ve been led astray.
+    somehow, I come back stronger,
+    every time I fall I’m down for longer,
+    no matter how hard someone beats me down, 
+    I’ll step it up and rise from the ground,
+    Never lost and never will,
+    Destined to win I will fulfill, 
+    The prophecy of pain, for myself, the crown,
+    I will stand and gain my ground,
+    Cant be beat and can’t be touched,
+    Thats when all the haters rush,
+    To beat me down, to win the round,
+    It’s at that time that i’ll be found,
+    broken, bare, and standing there,
+    arms up high  attack them quick,
+    they just keep fighting, they just can’t quit,
+    Soon enough they’ll see their graves,
+    It’s at that time they can’t be saved,
+    Depressed and angered, they just can’t choose,
+    Because finally they understand… I can’t lose.
+    "`
 },
 {
     id: 4,
